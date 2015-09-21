17ffd514cf386bc811abb58c58367c79da731352
Youssef Chaker <youssef.chaker@gmail.com>
2015-09-21 11:06:19 +0300

    Adds tests for the parseUrl function in the esUrlSvc service.

---
+++ test/spec/esUrlSvc.js
@@ -0,0 +1,44 @@
+'use strict';
+/*global describe,beforeEach,inject,it,expect*/
+describe('Service: esUrlSvc', function () {
+
+  beforeEach(module('o19s.splainer-search'));
+
+  var esUrlSvc;
+
+  beforeEach(inject(function (_esUrlSvc_) {
+    esUrlSvc = _esUrlSvc_;
+  }));
+
+  describe('parse URL', function() {
+    it('extracts the different parts of a URL', function() {
+      var url = 'http://localhost:9200/tmdb/_search';
+      esUrlSvc.parseUrl(url);
+
+      expect(esUrlSvc.protocol).toBe('http:');
+      expect(esUrlSvc.host).toBe('localhost:9200');
+      expect(esUrlSvc.pathname).toBe('/tmdb/_search');
+
+      var url = 'http://es.quepid.com/tmdb/_search';
+      esUrlSvc.parseUrl(url);
+
+      expect(esUrlSvc.protocol).toBe('http:');
+      expect(esUrlSvc.host).toBe('es.quepid.com');
+      expect(esUrlSvc.pathname).toBe('/tmdb/_search');
+
+      var url = 'https://es.quepid.com/tmdb/_search';
+      esUrlSvc.parseUrl(url);
+
+      expect(esUrlSvc.protocol).toBe('https:');
+      expect(esUrlSvc.host).toBe('es.quepid.com');
+      expect(esUrlSvc.pathname).toBe('/tmdb/_search');
+    });
+
+    it('adds http if the protocol is missing', function() {
+      var url = 'localhost:9200/tmdb/_search';
+      esUrlSvc.parseUrl(url);
+
+      expect(esUrlSvc.protocol).toBe('http:');
+    });
+  });
+});
