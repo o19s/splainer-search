42e9f1382158438f3e8ce4b5e9e69edb8eaae363
Doug Turnbull <softwaredoug@gmail.com>
2015-12-01 22:37:57 -0500

    Build splainer-search

--- splainer-search.js
+++ splainer-search.js
@@ -163,7 +163,7 @@ angular.module('o19s.splainer-search')
 'use strict';
 
 angular.module('o19s.splainer-search')
-  .service('esSearcherPreprocessorSvc', function esSearcherPreprocessorSvc() {
+  .service('esSearcherPreprocessorSvc', function esSearcherPreprocessorSvc(queryTemplateSvc) {
     var self      = this;
     self.prepare  = prepare;
 
@@ -174,7 +174,8 @@ angular.module('o19s.splainer-search')
       }
 
       var replaced  = angular.toJson(args, true);
-      replaced      = replaced.replace(/#\$query##/g, queryText);
+
+      replaced      = queryTemplateSvc.hydrate(replaced, queryText);
       replaced      = angular.fromJson(replaced);
 
       return replaced;
@@ -1037,6 +1038,52 @@ angular.module('o19s.splainer-search')
 
 'use strict';
 
+angular.module('o19s.splainer-search')
+  .service('queryTemplateSvc', function queryTemplateSvc() {
+    var self      = this;
+    self.hydrate = hydrate;
+
+    var defaultConfig = {
+      encodeURI: false
+    };
+
+    function encode(queryPart, config) {
+      if (config.encodeURI) {
+        return encodeURIComponent(queryPart);
+      } else {
+        return queryPart;
+      }
+    }
+
+
+    function keywordMapping(queryText) {
+      var queryTerms    = queryText.split(/[ ,]+/);
+      var maxKeywords = 10;
+      var numTerms = queryTerms.length;
+      for (var i = numTerms; i < maxKeywords; i++) {
+        queryTerms.push('');
+      }
+      return queryTerms;
+    }
+
+    function hydrate(template, queryText, config) {
+      if (!config) {
+        config = defaultConfig;
+      }
+
+      var replaced  = template.replace(/#\$query##/g, encode(queryText, config));
+      var idx = 0;
+      angular.forEach(keywordMapping(queryText), function(queryTerm) {
+        var regex = new RegExp('#\\$query' + (idx + 1) + '##', 'g');
+        replaced = replaced.replace(regex, encode(queryTerm, config));
+        idx += 1;
+      });
+      return replaced;
+    }
+  });
+
+'use strict';
+
 // Executes a solr search and returns
 // a set of solr documents
 angular.module('o19s.splainer-search')
@@ -1231,7 +1278,7 @@ angular.module('o19s.splainer-search')
 'use strict';
 
 angular.module('o19s.splainer-search')
-  .service('solrSearcherPreprocessorSvc', function solrSearcherPreprocessorSvc(solrUrlSvc, defaultSolrConfig) {
+  .service('solrSearcherPreprocessorSvc', function solrSearcherPreprocessorSvc(solrUrlSvc, defaultSolrConfig, queryTemplateSvc) {
     var self      = this;
     self.prepare  = prepare;
 
@@ -1272,7 +1319,7 @@ angular.module('o19s.splainer-search')
       }
 
       var baseUrl = solrUrlSvc.buildUrl(url, args);
-      baseUrl = baseUrl.replace(/#\$query##/g, encodeURIComponent(queryText));
+      baseUrl = queryTemplateSvc.hydrate(baseUrl, queryText, {encodeURI: true});
 
       return baseUrl;
     };
