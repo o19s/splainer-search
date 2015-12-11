66a07cfb70bbf400aa077c27d8083d085f4fcb68
Doug Turnbull <softwaredoug@gmail.com>
2015-12-11 15:22:31 -0500

    Simplifies regex
    Previous form was overly complex to achieve the desired effect as noted
    in the code review

--- services/queryTemplateSvc.js
+++ services/queryTemplateSvc.js
@@ -19,7 +19,7 @@ angular.module('o19s.splainer-search')
     }
 
     function getMaxKws(template) {
-      var keywordMatch = /#\$keyword(\d)(\|.*?){0,1}##/g;
+      var keywordMatch = /#\$keyword\d|(.*?)##/g;
       var match = keywordMatch.exec(template);
       var maxKw = 0;
       while (match !== null) {
@@ -47,7 +47,7 @@ angular.module('o19s.splainer-search')
       // Though its possible this link gets out of link, this was the origin
       // of the regex below
       // http://www.regexpal.com/?fam=93576
-      replaced = replaced.replace(/#\$keyword\d(\|(.*?)){1}##/g, '$2'); // regex
+      replaced = replaced.replace(/#\$keyword\d\|(.*?)##/g, '$1'); // regex
       // anything left, use config defaults
       replaced = replaced.replace(/#\$keyword\d(\|(.*?)){0,1}##/g, config.defaultKw); // regex
       return replaced;
