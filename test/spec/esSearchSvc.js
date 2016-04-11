baa0dca3e21c40488dabc0cdd0e4d03f6fa9c097
Doug Turnbull <softwaredoug@gmail.com>
2016-04-11 12:30:20 -0400

    Resolve conflict with master for esSearch tests

--- test/spec/esSearchSvc.js
+++ test/spec/esSearchSvc.js
@@ -104,9 +104,26 @@ describe('Service: searchSvc: ElasticSearch', function() {
       $httpBackend.verifyNoOutstandingExpectation();
       expect(called).toEqual(1);
     });
+    it('source has no "doc" property', function() {
+      $httpBackend.expectPOST(mockEsUrl).
+      respond(200, mockResults);
 
-    it('reports errors for the search', function() {
-      var errorMsg = 'your query just plain stunk';
+      var called = 0;
+
+      searcher.search()
+      .then(function() {
+        var docs = searcher.docs;
+        expect(docs[0].source().doc).toBe(undefined);
+        called++;
+      });
+      $httpBackend.flush();
+      $httpBackend.verifyNoOutstandingExpectation();
+      expect(called).toEqual(1);
+    });
+
+
+    it('reports pretty printed errors for ES errors but HTTP success', function() {
+      var errorMsg = {hits: [], _shards: {failed: 1, failures: [{foo: 'your query just plain stunk'}]}};
       $httpBackend.expectPOST(mockEsUrl).
       respond(400, errorMsg);
 
@@ -219,6 +236,23 @@ describe('Service: searchSvc: ElasticSearch', function() {
       $httpBackend.verifyNoOutstandingExpectation();
       expect(called).toEqual(1);
 
+    });
+
+    it('source has no _explanation', function() {
+      var mockResultsWithExpl = angular.copy(mockResults);
+      mockResultsWithExpl.hits.hits[0]._explanation = sumExplain;
+      var called = 0;
+      $httpBackend.expectPOST(mockEsUrl).
+        respond(200, mockResultsWithExpl);
+      searcher.search()
+      .then(function() {
+        var docs = searcher.docs;
+        expect(docs[0].source()._explanation).toBe(undefined);
+        called++;
+      });
+      $httpBackend.flush();
+      $httpBackend.verifyNoOutstandingExpectation();
+      expect(called).toEqual(1);
     });
   });
 
@@ -489,6 +523,20 @@ describe('Service: searchSvc: ElasticSearch', function() {
         expect(docs[1].highlight("AU8pXbemwjf9yCj9Xh4e", 'foo', '<b>', '</b>')).toEqual(expectedHl);
       });
 
+      $httpBackend.flush();
+      $httpBackend.verifyNoOutstandingExpectation();
+      expect(called).toBe(1);
+    });
+
+    it('source has no highlighting property', function() {
+      $httpBackend.expectPOST(mockEsUrl).respond(200, fullResponse);
+
+      var called = 0;
+      searcher.search().then(function() {
+        var docs = searcher.docs;
+        expect(docs[0].source().highlight).toBe(undefined);
+        called++;
+      });
       $httpBackend.flush();
       $httpBackend.verifyNoOutstandingExpectation();
       expect(called).toBe(1);
