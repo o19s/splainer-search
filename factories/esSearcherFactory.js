d0d246ca2c502ec9941a0e8e25a4498fcbcd5e36
Doug Turnbull <softwaredoug@gmail.com>
2018-10-23 16:29:46 -0400

    Fix CORS warning to support my training

--- factories/esSearcherFactory.js
+++ factories/esSearcherFactory.js
@@ -188,7 +188,7 @@
               errorMsg +=  '\n';
               errorMsg +=  'Enable CORS in elasticsearch.yml:\n';
               errorMsg += '\n';
-              errorMsg += 'http.cors.allow-origin: "/https?:\\/\\/(.*?\\.)?(quepid\\.com|splainer\\.io)/"';
+              errorMsg += 'http.cors.allow-origin: "/https?:\\\\/\\\\/(.*?\\\\.)?(quepid\\\\.com|splainer\\\\.io)/"';
               errorMsg += 'http.cors.enabled: true\n';
             }
             msg.searchError = errorMsg;
