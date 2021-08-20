a083c06a09b06a6b28eb725658bbde7f39b43a8e
Ryan Patterson <ryan@bigdataboutique.com>
2021-08-20 07:38:28 -1000

    Add support for scripted fields (#90)

--- services/esSearcherPreprocessorSvc.js
+++ services/esSearcherPreprocessorSvc.js
@@ -102,10 +102,6 @@ angular.module('o19s.splainer-search')
         }
       };
 
-      var setFieldsParamName = function() {
-        self.fieldsParamNames = [ '_source'];
-      };
-
       function prepare (searcher) {
         if (searcher.config === undefined) {
           searcher.config = defaultESConfig;
@@ -114,8 +110,6 @@ angular.module('o19s.splainer-search')
           // the default config object.
           searcher.config = angular.merge({}, defaultESConfig, searcher.config);
         }
-
-        setFieldsParamName(searcher);
 
         if ( searcher.config.apiMethod === 'post') {
           preparePostRequest(searcher);
